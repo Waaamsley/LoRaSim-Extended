9a452073743b1ceed918e2195ed09ce1c734193d
Waaamsley <walja829@student.otago.ac.nz>
2019-11-12 12:28:21 +1300

    Add files via upload

--- networkSupport.py
+++ networkSupport.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import time
 
+
 class nodePlacer():
 
     def __init__(self, nodes):
@@ -143,6 +144,7 @@ class fairSF():
 
         return sf_percentage
 
+
 class experiments():
 
     def __init__(self, experiment):
@@ -187,3 +189,36 @@ class experiments():
 
     def experimentEight(self):
         self.sf = 0
+
+
+class estimator:
+
+    def airtime(self, sf, cr, pl, bw):
+        H = 0  # implicit header disabled (H=0) or not (H=1)
+        DE = 0  # low data rate optimization enabled (=1) or not (=0)
+        Npream = 8  # number of preamble symbol (12.25  from Utz paper)
+
+        if bw == 125 and sf in [11, 12]:
+            # low data rate optimization mandated for BW125 with SF11 and SF12
+            DE = 1
+        if sf == 6:
+            # can only have implicit header with SF6
+            H = 1
+
+        Tsym = (2.0 ** sf) / bw
+        Tpream = (Npream + 4.25) * Tsym
+        print "sf", sf, " cr", cr, "pl", pl, "bw", bw
+        payloadSymbNB = 8 + max(math.ceil((8.0 * pl - 4.0 * sf + 28 + 16 - 20 * H) / (4.0 * (sf - 2 * DE))) * (cr + 4),
+                                0)
+        Tpayload = payloadSymbNB * Tsym
+        return Tpream + Tpayload
+
+    def chirpTime(self, sf, bw):
+        chirpyTime = (2 ** sf) / bw
+        return chirpyTime
+
+    # Okumura-Hata path loss model.
+    def hataUrban(self, sensi):
+        pathLoss = 17.5 - sensi
+        d = 10 ** ((pathLoss - 124.76) / 35.22)
+        print(d)
